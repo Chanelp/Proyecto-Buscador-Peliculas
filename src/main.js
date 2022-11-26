74c47e9daf8bc80304ed18bcf944cfba4b161e89
Chanelp <mariannissanchez27@gmail.com>
2022-11-25 21:34:03 -0400

    Agregando lista de categorías

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+//Función para renderizar la lista de películas en tendencia
 async function getTrendingsMoviesPreview() {
-  const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
+  const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY + "&language=es");
   const data = await res.json();
   const movies = data.results;
 
+  //Renderizar las películas en tendencia
   movies.forEach((movie) => {
     const trendingPreviewContent = document.querySelector('#trendingPreview .trendingPreview-movieList');
     const movieContent = document.createElement("div");
@@ -22,4 +24,29 @@ async function getTrendingsMoviesPreview() {
   });
 }
 
+//Función para renderizar la lista de categorías
+async function getCategoriesPreview() {
+  const res = await fetch("https://api.themoviedb.org/3//genre/movie/list?api_key=" + API_KEY + "&language=es");
+  const data = await res.json();
+  const categories = data.genres;
+
+  //Renderizar las categorías en tendencia
+  categories.forEach((category) => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    const CategoryContent = document.createElement("div");
+    CategoryContent.classList.add("category-container");
+
+    const CategoryTitle = document.createElement("h3");
+    CategoryTitle.setAttribute("id", "id" + category.id);
+    CategoryTitle.classList.add('category-title');
+    CategoryTitle.innerText = category.name;
+
+    CategoryContent.appendChild(CategoryTitle);
+    previewCategoriesContainer.appendChild(CategoryContent);
+  });
+}
+
+
+
 getTrendingsMoviesPreview();
+getCategoriesPreview();
