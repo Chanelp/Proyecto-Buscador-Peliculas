75c61eb852c769c20abe5c0f0afae0a970006bff
Chanelp <mariannissanchez27@gmail.com>
2022-11-25 20:32:36 -0400

    Agregando funcionalidad para traer la lista de películas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("Java script listo");
+async function getTrendingsMoviesPreview() {
+  const res = await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY);
+  const data = await res.json();
+  const movies = data.results;
+
+  movies.forEach((movie) => {
+    const trendingPreviewContent = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    const movieContent = document.createElement("div");
+    movieContent.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300/" + movie.poster_path
+    );
+    
+    movieImg.setAttribute("alt", "Imagen de la peli");
+    movieImg.classList.add("movie-img");
+
+    movieContent.appendChild(movieImg);
+    trendingPreviewContent.appendChild(movieContent);
+  });
+}
+
+getTrendingsMoviesPreview();
