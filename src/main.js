da786f03d6fbc2f2980d3d15f551e62ae1ad546d
Chanelp <mariannissanchez27@gmail.com>
2022-11-26 12:55:09 -0400

    Refactorizando algunas funciones repetidas

--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
 const api = axios.create({
-  baseURL : 'https://api.themoviedb.org/3/',
+  baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    'Content-Type': 'application/json; charset=utf-8'
+    "Content-Type": "application/json; charset=utf-8",
   },
   params: {
-    'api_key': API_KEY,
-    "language": "es-ES"
+    api_key: API_KEY,
+    language: "es-ES",
   },
 });
 
-//Función para renderizar la lista de películas en tendencia  
-async function getTrendingsMoviesPreview() {
-  const { data } = await api('trending/movie/day');
-  const movies = data.results;
+/*UTILS*/
 
-  trendingMoviesPreviewList.innerHTML = "";
-
-  //Renderizar las películas en tendencia
+function createMovies(movies, container) {
+  container.innerHTML = "";
   movies.forEach((movie) => {
     const movieContent = document.createElement("div");
     movieContent.classList.add("movie-container");
@@ -26,73 +22,68 @@ async function getTrendingsMoviesPreview() {
       "src",
       "https://image.tmdb.org/t/p/w300/" + movie.poster_path
     );
-    
+
     movieImg.setAttribute("alt", "Imagen de la peli");
     movieImg.classList.add("movie-img");
 
     movieContent.appendChild(movieImg);
-    trendingMoviesPreviewList.appendChild(movieContent);
+    container.appendChild(movieContent);
   });
 }
 
-//Función para renderizar la lista de categorías
-async function getCategoriesPreview() {
-  const { data } = await api('genre/movie/list');
-  const categories = data.genres;
+function createCategories(categories, container) {
+  container.innerHTML = "";
 
-  categoriesPreviewList.innerHTML = "";
-
-  //Renderizar las categorías en tendencia
   categories.forEach((category) => {
     const CategoryContent = document.createElement("div");
     CategoryContent.classList.add("category-container");
 
     const CategoryTitle = document.createElement("h3");
-    CategoryTitle.classList.add('category-title');
+    CategoryTitle.classList.add("category-title");
     CategoryTitle.setAttribute("id", "id" + category.id);
-    CategoryTitle.addEventListener('click', () => {
+    CategoryTitle.addEventListener("click", () => {
       location.hash = `#category=${category.id}-${category.name}`;
-    })
+    });
     const categoryTitleText = document.createTextNode(category.name);
 
     CategoryTitle.appendChild(categoryTitleText);
     CategoryContent.appendChild(CategoryTitle);
-    categoriesPreviewList.appendChild(CategoryContent);
+    container.appendChild(CategoryContent);
   });
+}
+
+/*LLAMADOS A LA API*/
+//Función para renderizar la lista de películas en tendencia
+async function getTrendingsMoviesPreview() {
+  const { data } = await api("trending/movie/day");
+  const movies = data.results;
+
+  //Renderizar las películas en tendencia
+  createMovies(movies, trendingMoviesPreviewList);
+}
+
+//Función para renderizar la lista de categorías
+async function getCategoriesPreview() {
+  const { data } = await api("genre/movie/list");
+  const categories = data.genres;
+
+  //Renderizar las categorías en tendencia - categoriesPreviewList
+  createCategories(categories, categoriesPreviewList);
 }
 
 //Función para filtrar películas por categoría
 async function getMoviesByCategory(id) {
-  const { data } = await api('discover/movie', {
+  const { data } = await api("discover/movie", {
     params: {
-      with_genres : id
+      with_genres: id,
     },
   });
 
   const movies = data.results;
 
-  genericSection.innerHTML = "";
-
   //Renderizar las películas por categoría
-  movies.forEach((movie) => {
-    const movieContent = document.createElement("div");
-    movieContent.classList.add("movie-container");
-
-    const movieImg = document.createElement("img");
-    movieImg.setAttribute(
-      "src",
-      "https://image.tmdb.org/t/p/w300/" + movie.poster_path
-    );
-    
-    movieImg.setAttribute("alt", "Imagen de la peli");
-    movieImg.classList.add("movie-img");
-
-    movieContent.appendChild(movieImg);
-    genericSection.appendChild(movieContent);
-  });
+  createMovies(movies, genericSection);
 }
-
-
 
 //getTrendingsMoviesPreview();
 //getCategoriesPreview();
